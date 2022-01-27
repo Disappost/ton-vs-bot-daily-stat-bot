839ccf4e6b8aedfcf1a6e88e07312f65e7bac029
disappost <disappost@pm.me>
2022-01-27 22:34:53 +0200

    add another channel_chat_id for testing

--- constants.py
+++ constants.py
@@ -3,4 +3,7 @@ heroku = True
 comments_in_console = False
 sleep_timeout = 60
 
-channel_chat_id = -1001585866898
+if heroku:
+    channel_chat_id = -1001585866898
+else:
+    channel_chat_id = -1001741577478
